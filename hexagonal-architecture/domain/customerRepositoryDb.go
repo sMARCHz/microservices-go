0b6829514656937e98100829d10c0b795bd533e0
Nattanon Chansamakr <mart_nattanon@hotmail.com>
2022-05-30 23:34:43 +0700

    Add logging

--- hexagonal-architecture/domain/customerRepositoryDb.go
+++ hexagonal-architecture/domain/customerRepositoryDb.go
@@ -2,11 +2,11 @@ package domain
 
 import (
 	"database/sql"
-	"log"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/sMARCHz/microservices-go/errs"
+	"github.com/sMARCHz/microservices-go/logger"
 )
 
 type CustomerRepositoryDb struct {
@@ -17,7 +17,7 @@ func (c CustomerRepositoryDb) FindAll() ([]Customer, *errs.AppError) {
 	findAllSql := "SELECT customer_id, name, city, zipcode, date_of_birth, status FROM customer"
 	rows, err := c.client.Query(findAllSql)
 	if err != nil {
-		log.Println("Error while querying customer table " + err.Error())
+		logger.Error("Error while querying customer table - " + err.Error())
 		return nil, errs.NewUnexpectedError("Unexpected database error")
 	}
 
@@ -26,7 +26,7 @@ func (c CustomerRepositoryDb) FindAll() ([]Customer, *errs.AppError) {
 		var customer Customer
 		err := rows.Scan(&customer.Id, &customer.Name, &customer.City, &customer.Zipcode, &customer.DateofBirth, &customer.Status)
 		if err != nil {
-			log.Println("Error while scanning customers " + err.Error())
+			logger.Error("Error while scanning customers - " + err.Error())
 			return nil, errs.NewUnexpectedError("Unexpected database error")
 		}
 		customers = append(customers, customer)
@@ -44,7 +44,7 @@ func (c CustomerRepositoryDb) FindById(id string) (*Customer, *errs.AppError) {
 		if err == sql.ErrNoRows {
 			return nil, errs.NewNotFoundError("Customer not found")
 		} else {
-			log.Println("Error while scanning customers " + err.Error())
+			logger.Error("Error while scanning customers - " + err.Error())
 			return nil, errs.NewUnexpectedError("Unexpected database error")
 		}
 	}
@@ -55,7 +55,7 @@ func (c CustomerRepositoryDb) FindByStatus(status string) ([]Customer, *errs.App
 	findByStatusSql := "SELECT customer_id, name, city, zipcode, date_of_birth, status FROM customer WHERE status = ?"
 	rows, err := c.client.Query(findByStatusSql, status)
 	if err != nil {
-		log.Println("Error while querying customer table " + err.Error())
+		logger.Error("Error while querying customer table - " + err.Error())
 		return nil, errs.NewUnexpectedError("Unexpected database error")
 	}
 
@@ -64,7 +64,7 @@ func (c CustomerRepositoryDb) FindByStatus(status string) ([]Customer, *errs.App
 		var customer Customer
 		err := rows.Scan(&customer.Id, &customer.Name, &customer.City, &customer.Zipcode, &customer.DateofBirth, &customer.Status)
 		if err != nil {
-			log.Println("Error while scanning customers " + err.Error())
+			logger.Error("Error while scanning customers - " + err.Error())
 			return nil, errs.NewUnexpectedError("Unexpected database error")
 		}
 		customers = append(customers, customer)
--- hexagonal-architecture/main.go
+++ hexagonal-architecture/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "github.com/sMARCHz/microservices-go/app"
+import (
+	"github.com/sMARCHz/microservices-go/app"
+	"github.com/sMARCHz/microservices-go/logger"
+)
 
 func main() {
+	logger.Info("Starting the application...")
 	app.Start()
 }
